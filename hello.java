8df89d7fef39ce7b55ca056f9ae05f7ec5520323
Thomas Barnette <tvbarnet@ncsu.edu>
2016-06-14 23:03:38 -0400

    made java file same size as C and HTML

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class Hi{
-	public static void main(String[] args){
+	public static void main(String[] arg){
 		System.out.println("Hello World");
+		System.out.println("Lets make this file the same size as HTML & C");
 	}
 }
